59177be26e22564f8de73f3be4a22713423b1088
cafier <cafier@users.noreply.github.com>
2016-06-28 08:33:57 +0800

    Update Information database.h

--- Information database.h
+++ Information database.h
@@ -9,18 +9,25 @@ class Information
 {
 	char barcode[MAX][10];//条形码 商品唯一标识 以字符串形式保存
 	char name[MAX][10];   //商品名称
-	
-	float price[MAX];          //单价
-	float discount[MAX];       //折扣价
 	char unit[MAX][4];
+	float price[MAX];          //单价
+	float discount1[MAX];       //折扣价
+	float discount[MAX];       //折扣	
 	int sum;              //库存商品种类
+		
 public:
-	Information(){sum=0;}
+	Information()
+	{
+		sum=0;
+		read();  
+	}
 	//录入商品信息
 	void input();
 	//输出商品信息
 	void output( );
 	//通过条形码，查看商品是否存在
+	void write();
+	void read();
 	int search(char barcodex[MAX]);
 	void returninformation(char *barcodex,char *namex,char *unitx,float &price,float &discount);
 };
@@ -49,12 +56,13 @@ void Information::input()
 
 		cout<<" input commodity price：";
 		cin>>price[i+sumx];
-		cout<<"input commodity discount price：";
+		cout<<"input commodity discount ：";
 		cin>>discount[i+sumx];
+		discount1[i+sumx]=price[i+sumx]*discount[i+sumx];
 		
 		sum++;
 	}
-	system("Cls");
+	write();
 }
 //条形码内容是库内商品码，返回真
 int Information::search(char barcodex[10])
@@ -72,23 +80,22 @@ int Information::search(char barcodex[10])
 }
 void Information::output()
 {
+	cout<<"commodity basic information：";
+	cout<<"code     name    unit name    price    discount price"<<endl;
 	for(int i=0;i<sum;i++)
 	{
-		cout<<"commodity basic information：";
-		cout<<"code     name    unit name    price    discount price"<<endl;
-		cout<<"                ";
-		printf("%s",barcode[i]);
+		printf("%6s",barcode[i]);
 		cout<<"   ";
-		printf("%s",name[i]);
+		printf("%8s",name[i]);
 		cout<<"   ";
-		printf("%s",unit[i]);
+		printf("%2s",unit[i]);
 		cout<<"   ";
 		cout<<price[i];
 		cout<<"   ";
-		cout<<discount[i]<<endl;
+		cout<<discount1[i]<<endl;
 	}
 }
-void Information::returninformation(char *barcodex,char *namex,char *unitx,float &pricex,float &discountx)
+void Information::returninformation(char *barcodex,char *namex,char *unitx,float &pricex,float &discount1x)
 {
 	for(int i=0;i<sum;i++)
 	{
@@ -97,8 +104,90 @@ void Information::returninformation(char *barcodex,char *namex,char *unitx,float
 			strcpy(namex,name[i]);
 			strcpy(unitx,unit[i]);
 			pricex=price[i];
-			discountx=discount[i];
+			discount1x=discount1[i];
 			break;
 		}
 	}
+}
+void Information::write()
+{
+	char x;
+	 FILE *fp;
+	 fp=fopen("d:\\file\\a.txt","w");
+	 if(fp==NULL)
+	 {
+		 printf("file inexistence！");
+		 exit(0);
+	 }
+	 fprintf(fp,"%d",sum);
+	 fprintf(fp,"%c",' ');
+	 for(int i=0;i<sum;i++)
+	 {
+		 fwrite(barcode[i],sizeof(char)*10,1,fp);
+		 fprintf(fp,"%c",' ');
+	 }
+	 for(int i=0;i<sum;i++)
+	 {
+		 fwrite(name[i],sizeof(char)*10,1,fp);
+		 fprintf(fp,"%c",' ');
+	 }
+	 for(int i=0;i<sum;i++)
+	 {
+		 fwrite(unit[i],sizeof(char)*4,1,fp);
+		 fprintf(fp,"%c",' ');
+	 }
+	  for(int i=0;i<sum;i++)
+	 {
+		 fprintf(fp,"%f",price[i]);
+	     fprintf(fp,"%c",' ');
+	 }
+	   for(int i=0;i<sum;i++)
+	 {
+		 fprintf(fp,"%f",discount[i]);
+	     fprintf(fp,"%c",' ');
+	 }
+     fclose(fp);
+
+	 
+}
+void Information::read()
+{
+	char x;
+	FILE *fp;
+
+	fp=fopen("d:\\file\\a.txt","r");
+	 if(fp==NULL)
+	 {
+		 printf(" file inexistence！");
+		 exit(0);
+	 }
+	
+	 fscanf(fp,"%d",&sum);
+	 fscanf(fp,"%c",&x);
+	 for(int i=0;i<sum;i++)
+	 {
+		 fread(barcode[i],sizeof(char)*10,1,fp);
+		 fscanf(fp,"%c",&x);
+	 }
+	 for(int i=0;i<sum;i++)
+	 {
+		 fread(name[i],sizeof(char)*10,1,fp);
+		 fscanf(fp,"%c",&x);
+	 }
+	 for(int i=0;i<sum;i++)
+	 {
+		 fread(unit[i],sizeof(char)*4,1,fp);
+		 fscanf(fp,"%c",&x);
+	 }
+	 for(int i=0;i<sum;i++)
+	 {
+		 fscanf(fp,"%f",&price[i]);
+		 fscanf(fp,"%c",&x);
+	 }
+	 for(int i=0;i<sum;i++)
+	 {
+		 fscanf(fp,"%f",&discount[i]);
+		 fscanf(fp,"%c",&x);
+	 }
+	 fclose(fp);
 }
